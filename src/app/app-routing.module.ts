935f609b0c180812166d5a9012df9611f86bc1f8
Jose Carlos Nuñez Valverde <josek21nunez@gmail.com>
2019-03-28 23:09:39 -0600

    Feature: Add component : main, errorPage and registry; also add the routing and check the functioning of this

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  // Error Page Not Found
+  { path: 'errorPage', component: ErrorPageComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'pageNotFound' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
